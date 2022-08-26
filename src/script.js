73843e00f4f6e0415cfbfcd1f2f593deacd7f0be
Shioridory <saeshio75@gmail.com>
2022-08-26 21:56:24 +0900

    Updated time and day

--- src/script.js
+++ src/script.js
@@ -1,23 +1,41 @@
 let now = new Date();
 
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
 
-let day = days[now.getDay()];
-let hours = now.getHours();
-let minutes = now.getMinutes();
-let dayTime = `${day} ${hours}:${minutes}`;
-let currentDayTime = document.querySelector("#current-day-time");
-currentDayTime.innerHTML = dayTime;
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
 
 function showTemperature(response) {
+  document.querySelector("#current-day-time").innerHTML = formatDate(
+    response.data.dt * 1000
+  );
+
   document.querySelector("#current-city").innerHTML = response.data.name;
   document.querySelector("#temp").innerHTML = Math.round(
     response.data.main.temp
