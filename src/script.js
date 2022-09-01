801c560ab843e54ed5880eeaec9c72935cc2ec70
Shioridory <saeshio75@gmail.com>
2022-09-01 12:19:07 +0900

    Added the function of changing background color depending on weather

--- src/script.js
+++ src/script.js
@@ -46,6 +46,8 @@ function showTemperature(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+
+  changeBackgroundColor(response.data.weather[0].description);
 }
 
 function searchCity(city) {
@@ -78,3 +80,104 @@ form.addEventListener("submit", handleSubmit);
 
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
+
+function changeBackgroundColor(description) {
+  if (description === "clear sky") {
+    document.querySelector(".weather-app").style.background = "##FFD774";
+  } else if (description === "few clouds") {
+    document.querySelector(".weather-app").style.background = "#92B3C2";
+  } else if (description === "scattered clouds") {
+    document.querySelector(".weather-app").style.background = "#92B3C2";
+  } else if (description === "broken clouds") {
+    document.querySelector(".weather-app").style.background = "#92B3C2";
+  } else if (description === "overcast clouds") {
+    document.querySelector(".weather-app").style.background = "#92B3C2";
+  } else if (description === "light rain") {
+    document.querySelector(".weather-app").style.background = "#0E86D4";
+  } else if (description === "moderate rain") {
+    document.querySelector(".weather-app").style.background = "#0E86D4";
+  } else if (description === "heavy intensity rain") {
+    document.querySelector(".weather-app").style.background = "#0E86D4";
+  } else if (description === "very heavy rain") {
+    document.querySelector(".weather-app").style.background = "#0E86D4";
+  } else if (description === "extreme rain") {
+    document.querySelector(".weather-app").style.background = "#0E86D4";
+  } else if (description === "freezing rain") {
+    document.querySelector(".weather-app").style.background = "#0E86D4";
+  } else if (description === "light intensity shower rain") {
+    document.querySelector(".weather-app").style.background = "#0E86D4";
+  } else if (description === "shower rain") {
+    document.querySelector(".weather-app").style.background = "#0E86D4";
+  } else if (description === "heavy intensity shower rain") {
+    document.querySelector(".weather-app").style.background = "#0E86D4";
+  } else if (description === "ragged shower rain") {
+    document.querySelector(".weather-app").style.background = "#0E86D4";
+  } else if (description === "mist") {
+    document.querySelector(".weather-app").style.background = "#C3D6DB";
+  } else if (description === "light snow") {
+    document.querySelector(".weather-app").style.background = "#fff";
+  } else if (description === "Snow") {
+    document.querySelector(".weather-app").style.background = "#fff";
+  } else if (description === "Heavt snow") {
+    document.querySelector(".weather-app").style.background = "#fff";
+  } else if (description === "Sleet") {
+    document.querySelector(".weather-app").style.background = "#fff";
+  } else if (description === "Light shower sleet") {
+    document.querySelector(".weather-app").style.background = "#fff";
+  } else if (description === "Shower sleet") {
+    document.querySelector(".weather-app").style.background = "#fff";
+  } else if (description === "Light rain and snow") {
+    document.querySelector(".weather-app").style.background = "#fff";
+  } else if (description === "Rain and snow") {
+    document.querySelector(".weather-app").style.background = "#fff";
+  } else if (description === "Light shower snow") {
+    document.querySelector(".weather-app").style.background = "#fff";
+  } else if (description === "Shower snow") {
+    document.querySelector(".weather-app").style.background = "#fff";
+  } else if (description === "Heavy shower snow") {
+    document.querySelector(".weather-app").style.background = "#fff";
+  } else if (description === "fog") {
+    document.querySelector(".weather-app").style.background = "#BBC4C2";
+  } else if (description === "light intensity drizzle") {
+    document.querySelector(".weather-app").style.background = "#68BBE3";
+  } else if (description === "drizzle") {
+    document.querySelector(".weather-app").style.background = "#68BBE3";
+  } else if (description === "heavy intensity drizzle") {
+    document.querySelector(".weather-app").style.background = "#68BBE3";
+  } else if (description === "light intensity drizzle rain") {
+    document.querySelector(".weather-app").style.background = "#68BBE3";
+  } else if (description === "drizzle rain") {
+    document.querySelector(".weather-app").style.background = "#68BBE3";
+  } else if (description === "heavy intensity drizzle rain") {
+    document.querySelector(".weather-app").style.background = "#68BBE3";
+  } else if (description === "shower rain and drizzle") {
+    document.querySelector(".weather-app").style.background = "#68BBE3";
+  } else if (description === "heavy shower rain and drizzle") {
+    document.querySelector(".weather-app").style.background = "#68BBE3";
+  } else if (description === "shower drizzle") {
+    document.querySelector(".weather-app").style.background = "#68BBE3";
+  } else if (description === "thunderstorm with light rain") {
+    document.querySelector(".weather-app").style.background = "#9B9896";
+  } else if (description === "thunderstorm with rain") {
+    document.querySelector(".weather-app").style.background = "#9B9896";
+  } else if (description === "thunderstorm with heavy rain") {
+    document.querySelector(".weather-app").style.background = "#9B9896";
+  } else if (description === "light thunderstorm") {
+    document.querySelector(".weather-app").style.background = "#9B9896";
+  } else if (description === "thunderstorm") {
+    document.querySelector(".weather-app").style.background = "#9B9896";
+  } else if (description === "heavy thunderstorm") {
+    document.querySelector(".weather-app").style.background = "#9B9896";
+  } else if (description === "ragged thunderstorm") {
+    document.querySelector(".weather-app").style.background = "#9B9896";
+  } else if (description === "thunderstorm with light drizzle") {
+    document.querySelector(".weather-app").style.background = "#9B9896";
+  } else if (description === "thunderstorm with drizzle") {
+    document.querySelector(".weather-app").style.background = "#9B9896";
+  } else if (description === "thunderstorm with heavy drizzle") {
+    document.querySelector(".weather-app").style.background = "#9B9896";
+  } else if (description === "Haze") {
+    document.querySelector(".weather-app").style.background = "#B9B7BD";
+  }
+  axios.get(apiUrl).then(showTemperature);
+}
